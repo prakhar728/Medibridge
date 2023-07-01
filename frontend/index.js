0289030d5bf048e6a4248f15688b666cafc6c33a
Prakhar <prakharojha12@gmail.com>
2023-07-01 19:58:05 +0530

    Added React router and Readme

--- frontend/index.js
+++ frontend/index.js
@@ -2,22 +2,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { createRoot } from 'react-dom/client';
 // NEAR
 import { Wallet } from './near-wallet';
+import LandingPage from './pages/LandingPage';
 
 const CONTRACT_ADDRESS = process.env.CONTRACT_NAME
 
+
 // When creating the wallet you can optionally ask to create an access key
 // Having the key enables to call non-payable methods without interrupting the user to sign
-const wallet = new Wallet({ createAccessKeyFor: CONTRACT_ADDRESS })
+const wallet = new Wallet({ createAccessKeyFor: CONTRACT_ADDRESS });
 
+
+const container = document.getElementById('root');
+const root = createRoot(container);
 // Setup on page load
 window.onload = async () => {
-  const isSignedIn = await wallet.startUp()
- 
-  ReactDOM.render(
-    <App isSignedIn={isSignedIn} contractId={CONTRACT_ADDRESS} wallet={wallet} />,
-    document.getElementById('root')
-  );
+  const isSignedIn = await wallet.startUp();
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/app",
+      element: <App isSignedIn={isSignedIn} contractId={CONTRACT_ADDRESS} wallet={wallet} />,
+    },
+  ]);
+  root.render(<RouterProvider router={router} />);
 }
